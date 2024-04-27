a872e80f7fa92b8f0630db87282ae7671c059ff1
Abdulkabir Omotoso <aomotoso@fsdhgroup.com>
2024-04-27 07:43:35 +0100

    Heap Insert method

---
+++ Heap/Heap.cs
@@ -0,0 +1,50 @@
+﻿namespace data_structure_and_algo.Heaps
+{
+    public class Heap
+    {
+        int[] items = new int[30];
+        int size = 0;
+
+
+        public void Insert(int num)
+        {
+            IsFull();
+
+            items[size++] = num;
+
+            BubbleUp();
+        }
+        private void BubbleUp()
+        {
+            int index = size - 1;
+            while (index > 0 && items[index] > items[Parent(index)])
+            {
+                Swap(index, Parent(index));
+                index = Parent(index);
+            }
+        }
+
+        private void IsFull()
+        {
+            if (size == items.Length)
+            {
+                throw new Exception("Illegal State Exception");
+            }
+        }
+        private int Parent(int index)
+        {
+            return (index - 1) / 2;
+        }
+        private void Swap(int firstIndex, int secondIndex)
+        {
+            int temp = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = temp;
+        }
+
+        public void ShowValues()
+        {
+            Console.Write(string.Join(", ", items));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿//Heap
+
+using data_structure_and_algo.Heaps;
+
+var heap = new Heap();
+
+heap.Insert(20);
+heap.Insert(30);
+heap.Insert(10);
+heap.Insert(90);
+heap.Insert(100);
+heap.Insert(60);
+heap.Insert(80);
+heap.Insert(200);
+heap.ShowValues();
